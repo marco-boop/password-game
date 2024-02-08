95efd73839cbe75b061e3dcc0002a4b1bba9e85c
Marco <sanchez.junco@gmail.com>
2024-02-08 10:50:55 -0800

    added requirements and win state

--- src/App.js
+++ src/App.js
@@ -2,63 +2,87 @@ import React, { useState } from "react";
 import "./index.css"
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
     const [input, setInput] = useState("");
     const [error, setError] = useState(false);
     const [reqs, setReqs] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [win, setWin] = useState(false);
 
     const checkInput = value => {
-
         let shouldError = false;
-
         let updatedReqs = [];
 
-        if (value.length >= 5) {
-            updatedReqs.push("Has 5+ chars");
-        }
-        if (/\d/.test(value)) {
-            updatedReqs.push("Contains a number");
-        }
-        if (/[a-z]/.test(value) && /[A-Z]/.test(value)) {
-            updatedReqs.push("Has upper and lowercase letters");
+        if (value.length < 5) {
+            shouldError = true;
+            setErrorMessage("Your password must be at least 5 characters long");
+        } else {
+            updatedReqs.push("Your password contains at least 5 characters");
+            setErrorMessage("");
+            if (!/\d/.test(value)) {
+                shouldError = true;
+                setErrorMessage("Your password must contain a number");
+            } else {
+                updatedReqs.push("Your password contains a number");
+                setErrorMessage("");
+                if (!/[a-z]/.test(value) || !/[A-Z]/.test(value)) {
+                    shouldError = true;
+                    setErrorMessage("Your password must contain upper and lowercase letters");
+                } else {
+                    updatedReqs.push("Your passwordh as upper and lowercase letters");
+                    setErrorMessage("");
+                }
+            }
         }
 
+        setError(shouldError);
         setReqs(updatedReqs);
 
+        if (!shouldError && updatedReqs.length === 3) {
+            setWin(true);
+        } else {
+            setWin(false);
+        }
     }
-
-
 
     return (
         <div className="bg-teal-100 h-full p-20">
             <div className="flex justify-center items-center">
                 <h1 className="text-5xl font-bold pb-10">The Password Game</h1>
             </div>
-            <ul>Requirements:
-                <li>- Has 5+ chars</li>
-                <li>- Contains a number</li>
-                <li>- Has upper and lowercase letters</li>
-            </ul>
-            <p className="my-5">You typed: {input}</p>
             <div className="flex flex-col justify-center items-center">
                 <p className="my-5">Please choose a password:</p>
                 <input
+                    className={`border border-gray-400 rounded p-2 text-gray-900 w-full text-center`}
                     type="text"
                     value={input}
                     onChange={(e) => {
                         setInput(e.target.value);
                         checkInput(e.target.value);
                     }}
-                    className={`border border-gray-400 rounded p-2 text-gray-900`}
+
                 />
-                {error && <p>Your password must be at least 5 characters long</p>}
-                <div className="mt-2">
-                    {reqs.map(req => (
-                        <p className="text-green-500" key={req}>{req}: check!</p>
-                    ))}
-                </div>
+                {error && <div className="bg-red-200 p-5 shadow-2xl my-10 rounded-lg w-full flex items-center">
+                    <span class="relative flex h-3 w-3">
+                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                    </span>
+                    <p className="ml-3">{errorMessage}</p>
+                </div>}
+
+                {win && <div className="bg-green-200 p-5 shadow-2xl my-10 rounded-lg w-full flex items-center">
+                    <span class="relative flex h-3 w-3">
+                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                    </span>
+                    <p className="ml-3">You win! So safe!</p>
+                </div>}
+
+                {reqs.map(req => (
+                    <div className="w-full flex items-center bg-white mt-2 p-5 shadow-2xl my-7 rounded-lg ring-offset-4 ring-green-600" key={req}>{req}: check!</div>
+                ))}
+
             </div>
-        </div>
+        </div >
     );
 };
 
